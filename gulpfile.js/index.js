eaa6e1d61f6b69a9238c90fb2a3616468c659183
Max Edelstein <edelsteinm@cmoa.org>
2019-11-20 08:46:33 -0500

    abstracted tasks to their own files using module.exports

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,37 +1,13 @@
-// this file is ran when you type $ gulp in the command line
+/*
+  idea is to not have one large gulp file, rather each file will
+  handle a specific task.
 
-var gulp = require('gulp');
+  to create a new task, add a new fileName.extension to the task directory
+  gulpfile.js/tasks/default.js is ran when you type $gulp in the cli
 
-function defaultGulpTask() {
-  // watch code files for any changes
-  watch();
-}
+*/
 
-// to use $ gulp which will run the default task
-exports.default = defaultGulpTask;
+var requireDirectory = require('require-dir');
 
-// to use browserSync methods
-var browserSync = require('browser-sync').create();
-
-// watch for changes
-function watch() {
-  browserSync.init({
-
-    // tell browsersync to use this dir and serve it as a mini-server
-    server: {
-      baseDir: './',
-      index: 'index.html',
-    }
-
-  });
-
-  // watch html for changes
-  gulp.watch('*.html').on('change', browserSync.reload);
-  // watch css for changes
-  gulp.watch('css/styles.css').on('change', browserSync.reload);
-  // watch javascript for changes
-  gulp.watch('scripts/*.js').on('change', browserSync.reload);
-  // watch gulpfile 'index.js' for changes
-  gulp.watch('gulpfile.js/*.js').on('change', browserSync.reload);
-
-}
+// require tasks from the task directory and subdirectorys
+requireDirectory('./tasks', {recurse: true} );
---
+++ gulpfile.js/tasks/browserSync.js
@@ -0,0 +1,28 @@
+// to use browserSync methods
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+
+// browserSync watch for changes
+var browserSyncWatch = function () {
+  browserSync.init({
+
+    // tell browsersync to use this dir and serve it as a mini-server
+    server: {
+      baseDir: './',
+      index: 'index.html',
+    }
+
+  });
+
+  // watch html for changes
+  gulp.watch('*.html').on('change', browserSync.reload);
+  // watch css for changes
+  gulp.watch('css/styles.css').on('change', browserSync.reload);
+  // watch javascript for changes
+  gulp.watch('scripts/*.js').on('change', browserSync.reload);
+  // watch gulpfile 'index.js' for changes
+  gulp.watch('gulpfile.js/*.js').on('change', browserSync.reload);
+
+}
+
+module.exports = browserSyncWatch;
