bd4774dddeddc92c94adcab46352345c15c0bc38
Max Edelstein <edelsteinm@cmoa.org>
2019-11-21 08:38:08 -0500

    reorganized code for clarity

--- gulpfile.js/tasks/browserSync.js
+++ gulpfile.js/tasks/browserSync.js
@@ -2,12 +2,6 @@
 var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 
-// will include methods from javascript.js
-var javascriptTask = require('./javascript.js');
-
-// will include methods from css.js
-var cssTask = require('./css.js');
-
 // browserSync watch for changes
 var browserSyncWatch = function () {
   browserSync.init({
@@ -22,14 +16,23 @@ var browserSyncWatch = function () {
 
   // watch all .html files from the root directory for changes
   gulp.watch('*.html').on('change', browserSync.reload);
+
+  // will include methods from css.js
+  var cssTask = require('./css.js');
+
   // watch all .css files in the styles directory for changes
   gulp.watch('css/styles.css').on('change', gulp.series(cssTask, browserSync.reload));
+
+  // will include methods from javascript.js
+  var javascriptTask = require('./javascript.js');
+
   // on changed javascript, bundle the javascript then reload the browser
   gulp.watch('scripts/input.js').on('change', gulp.series(javascriptTask, browserSync.reload));
+
   // watch gulpfile 'index.js' for changes
   gulp.watch('gulpfile.js/*.js').on('change', browserSync.reload);
 
-}
+} // end of browserSyncWatch function variable
 
 // export the browserSyncWatch function to be used in default.js
 module.exports = browserSyncWatch;
