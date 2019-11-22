861616499e1c98ac9e9f9a1c0881c8b6ec43e43e
Max Edelstein <edelsteinm@cmoa.org>
2019-11-22 08:49:30 -0500

    added code to concat js files

--- gulpfile.js/tasks/javascript.js
+++ gulpfile.js/tasks/javascript.js
@@ -2,9 +2,10 @@ var concat = require('gulp-concat');
 var gulp = require('gulp');
 
 var javascriptTask = function () {
-  return gulp.src('./scripts/input.js') // files to be bundled
-    .pipe(concat('output.js')) // bundled fileName.js
-    .pipe(gulp.dest('production/scripts/')); // directory fileName.js will be placed in
+  // files passed in the array will be concatenated in that order
+  return gulp.src(['./scripts/input.js', './scripts/inputTwo.js']) // files to be bundles
+    .pipe(concat('output.js')) // bundled output.js
+    .pipe(gulp.dest('production/scripts/')); // directory output.js will be placed in
 };
 
 gulp.task('javascript', javascriptTask);
---
+++ scripts/inputTwo.js
@@ -0,0 +1,1 @@
+console.log('inputTwo.js was concatenated second');
