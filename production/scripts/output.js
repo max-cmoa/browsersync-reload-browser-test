9d197b1b722b82c3d4fe953c0596c52de3362acf
Max Edelstein <edelsteinm@cmoa.org>
2019-11-26 08:57:01 -0500

    .gitignore is now working

--- production/scripts/output.js
+++
@@ -1,5 +0,0 @@
-console.log('changeddd input.js in "scripts/input.js" then save and look in the browsers developers console. Note how index.html file uses output.js');
-
-console.log('last.js was rendered last');
-
-console.log('anotherLast.jssssssssss was rendered last');
