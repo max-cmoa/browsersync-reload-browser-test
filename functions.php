0f21edd710bcdaf1a684a53810e66a39c570c359
Max Edelstein <edelsteinm@cmoa.org>
2019-12-04 14:18:56 -0500

    adds proper way to enqueue css

--- functions.php
+++ functions.php
@@ -1,10 +1,15 @@
 <?php
 
+// Add Theme Support
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails', [ 'post', 'page' ] );
+
 // enqueue our main.css file
 function enqueue_css() {
 
-    wp_enqueue_style( 'main-css', get_stylesheet_uri() . '/production/assets/sass/main.css', time(), 'all' );
-
+    wp_enqueue_style( 'main', get_template_directory_uri() . '/production/assets/sass/main.css', [], time(), 'all' );
+    //wp_enqueue_style( 'main', get_template_directory_uri() . '/production/assets/sass/main.css');
 }
 
-add_action('wp_enqueue_scripts', 'enqueue_css');
+// hook to connect wp execution process to our function
+add_action('wp_enqueue_scripts', 'enqueue_css', 10);
--- index.php
+++ index.php
@@ -1,9 +1,20 @@
+<html>
+
+ <head>
+  <?php wp_head(); ?>
+
+     <link rel="stylesheet" type="text/css" href="/production/assets/sass/main.css">
+ </head>
+
 <?php
 /*
   This file should be the only php fallback template for the simple-starter-theme.
   Relying on the WordPress template hierarchy can be a mess to trace.
 */
-
-  echo ('<div body_class(); >Sample text</div>');
-
 ?>
+
+<p class='standardParagraph'>hi</p>
+
+<?php echo (get_template_directory_uri() . '/production/assets/sass/main.css' ); ?>
+
+</html>
