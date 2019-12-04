aa2169c0420a9e5b45c19e9b70e2c5311534d137
Max Edelstein <edelsteinm@cmoa.org>
2019-12-04 14:38:07 -0500

    tidied up index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
+<!DOCTYPE html>
 <html>
  <!-- This should be the only fallback template -->
  <head>
+   <meta charset="utf-8">
+   <title>simple-starter-theme</title>
 
   <!-- use css and javascript enqueued in functions.php -->
   <!-- Open the console > stylesheets > main.css -->
@@ -8,7 +11,6 @@
   <?php wp_head(); ?>
 
  </head>
-
   <body>
 
    <p class='standardParagraph'>hi</p>
