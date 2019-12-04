4db15ff392c1f51402201523de201ef7d74d1d7c
Max Edelstein <edelsteinm@cmoa.org>
2019-12-04 11:54:17 -0500

    adds simple html output in index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<?php
 /*
   This file should be the only php fallback template for the simple-starter-theme.
   Relying on the WordPress template hierarchy can be a mess to trace.
 */
+
+  echo ('<div>Sample text</div>');
+
+?>
