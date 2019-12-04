aaf405410221cceee69f29434d1a9f93b470adcf
Max Edelstein <edelsteinm@cmoa.org>
2019-12-04 12:13:25 -0500

    fixed broken css.js gulp task

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -1,12 +1,12 @@
 /*
-    gulp the styles.css for production
+    gulp the style.css WordPress config file for production
 */
 
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 
 var cssTask = function() {
-  return gulp.src('./styles.css') // files to be bundled
+  return gulp.src('./style.css') // files to be bundled
     .pipe(cleanCSS()) // minify the css
     .pipe(gulp.dest('production/')); // directory to be placed in
 };
