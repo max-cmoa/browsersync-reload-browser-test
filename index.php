7200e640c3780d9d1d190625236343f49e517287
Max Edelstein <edelsteinm@cmoa.org>
2019-12-04 14:58:12 -0500

    adds comments to index.php

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
   <footer>
     <!-- use javascript enqueued in functions.php -->
     <!-- Open the console > stylesheets > main.js -->
+    <!-- You will see javascript WordPress also enqueues by default -->
     <?php wp_footer(); ?>
   </footer>
 </html>
