80cbe511da576120f43ced64bfb3d324e8a8fd6b
Max Edelstein <edelsteinm@cmoa.org>
2019-12-06 09:34:39 -0500

    prefixed functions with theme name in functions.php file

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
 <?php
 
+/*
+   Used to make files more modular. This files functionality
+   is to make the sass and javascript accessible to this theme.
+*/
+
 // Add Theme Support
 add_theme_support( 'title-tag' );
 add_theme_support( 'post-thumbnails', [ 'post', 'page' ] );
 
 // enqueue our main.css file, has to be in the header
-function enqueue_css() {
+function simple_starter_theme_enqueue_css() {
 
       // main represents main.css, next arg is path to main.css, followed by empty array, time() so no cache issues, and all for all media
     wp_enqueue_style( 'main', get_template_directory_uri() . '/production/assets/sass/main.css', [], time(), 'all' );
@@ -13,10 +18,10 @@ function enqueue_css() {
 }
 
 // hook to connect wp execution process to our function
-add_action('wp_enqueue_scripts', 'enqueue_css');
+add_action('wp_enqueue_scripts', 'simple_starter_theme_enqueue_css');
 
 // enqueue our main.js file
-function enqueue_javascript() {
+function simple_starter_theme_enqueue_javascript() {
 
   // main represents main.js, next arg is path to main.js, followed by empty array, time() so no cache issues, and true so javascript is in the footer
   wp_enqueue_script('main', get_template_directory_uri() . '/production/assets/javascript/main.js', [], time(), true);
@@ -24,4 +29,4 @@ function enqueue_javascript() {
 }
 
 // hook to connect wp execution process to our function
-add_action('wp_enqueue_scripts', 'enqueue_javascript');
+add_action('wp_enqueue_scripts', 'simple_starter_theme_enqueue_javascript');
