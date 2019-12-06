6ddeff35ef97954062e0d716562fe1a1eef8bc55
Max Edelstein <edelsteinm@cmoa.org>
2019-12-06 11:48:24 -0500

    got zebra-post working

--- post-templates/zebra-post.php
+++ post-templates/zebra-post.php
@@ -10,4 +10,18 @@
   Template > select zebra from drop down.
 */
 
+  get_template_part('../partial-templates/headers/main-header');
+
  ?>
+
+ <?php if( have_posts() ) : ?>
+   <?php while (have_posts() ) : ?>
+     <?php the_post(); ?>
+     <h1 class='standardParagraph'><?php the_content(); ?></h1>
+   <?php endwhile; else : ?>
+     <?php echo('No page content to display'); ?>
+   <?php endif; ?>
+
+<?php
+     get_template_part('../partial-templates/footers/main-footer');
+?>
