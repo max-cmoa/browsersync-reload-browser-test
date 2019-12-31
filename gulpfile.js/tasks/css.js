1d61883540f03cdabdba780bf33f6de907408b97
Max Edelstein <edelsteinm@cmoa.org>
2019-12-31 08:11:33 -0500

    fixed issue with main.css being gulped to the wrong location in production

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -7,7 +7,7 @@ var gulp = require('gulp');
 
 var cssTask = function() {
   return gulp.src('assets/css/main.css') // files to be bundled
-    .pipe(gulp.dest('production/')); // directory to be placed in
+    .pipe(gulp.dest('production/assets/css')); // directory to be placed in
 };
 
 gulp.task('cssTask', cssTask);
